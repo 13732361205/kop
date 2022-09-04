6e1782acf77a3789c996fd9f267d5f064fa9377c
xiezhihao1@qq.com <xiezhihao1@qq.com>
2022-09-04 11:25:01 +0800

    fix:  修改代码防止启动报错

--- rbac/rbac-manager/src/main/java/com/github/kop/rbac/api/UserController.java
+++ rbac/rbac-manager/src/main/java/com/github/kop/rbac/api/UserController.java
@@ -13,6 +13,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-  @Autowired private UserService userService;
+
+  @Autowired
+  @Qualifier("userServiceImpl")
+  private UserService userService;
   @Autowired private CompanyUserService companyUserService;
 
   @ApiOperation(value = "创建用户")
--- rbac/rbac-manager/src/test/java/com/github/kop/rbac/service/UserServlceTest.java
+++ rbac/rbac-manager/src/test/java/com/github/kop/rbac/service/UserServlceTest.java
@@ -1,16 +1,27 @@
-// package com.github.kop.rbac.service;
-//
-// import com.github.kop.rbac.RbacApplication;
-// import com.github.kop.rbac.module.req.user.CreateUserReq;
-// import org.junit.jupiter.api.Assertions;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-//
-// @SpringBootTest(classes = {RbacApplication.class})
-// public class UserServlceTest {
-//    @Autowired private CompanyService companyService;
-//    @Autowired private CompanyUserService companyUserService;
-//
+ package com.github.kop.rbac.service;
+
+ import com.github.kop.rbac.RbacApplication;
+ import com.github.kop.rbac.module.req.user.CreateUserReq;
+ import com.github.kop.rbac.service.impl.UserServiceImpl;
+ import org.junit.jupiter.api.Assertions;
+ import org.junit.jupiter.api.Test;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.beans.factory.annotation.Qualifier;
+ import org.springframework.beans.factory.annotation.Value;
+ import org.springframework.boot.test.context.SpringBootTest;
+
+ import javax.annotation.Resources;
+
+ @SpringBootTest(classes = {RbacApplication.class})
+ public class UserServlceTest {
+    @Autowired private CompanyService companyService;
+    @Autowired private CompanyUserService companyUserService;
+
+     @Autowired
+     @Qualifier("userServiceImpl")
+     private UserService userService;
+
+
 //    @org.junit.jupiter.api.Test
 //    void createUser(){
 //        CreateUserReq createUserReq=new CreateUserReq();
@@ -23,9 +34,18 @@
 //
 //        Assertions.assertNotNull(user);
 //    }
-//
-//
-//
-//    @org.junit.jupiter.api.Test
-//    void byId() {}
-// }
+
+
+
+     @org.junit.jupiter.api.Test
+     void login(){
+
+         String a="xiezhihao1@qq.com"  ;
+         String p="zhihaoAKB48";
+         long c=1564898952862621698L;
+         userService.login(a,p,c);
+     }
+
+    @org.junit.jupiter.api.Test
+    void byId() {}
+ }
